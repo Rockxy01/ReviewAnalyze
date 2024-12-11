214cfe8617757179b2f27251b4d9225cfeffda87
Rockxy01 <khokhar01.ranveer@gmail.com>
2024-12-11 13:00:58 +0530

    Update App.py

--- App.py
+++ App.py
@@ -24,6 +24,9 @@ def setup_driver():
     options = Options()
     options.add_argument("--headless")  # run in headless mode (no UI)
     options.add_argument(f"user-agent={get_random_user_agent()}")
+    chrome_options.binary_location = "/usr/bin/chromium-browser"
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage') 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     return driver
 
