751653c83a93ee29ca0eff5a610833582e897003
Rockxy01 <khokhar01.ranveer@gmail.com>
2024-12-13 11:32:53 +0530

    Update App.py

--- App.py
+++ App.py
@@ -55,7 +55,7 @@ def extract_amazon_reviews(url):
 
     # Extract product rating
     try:
-        rating = WebDriverWait(driver, 15).until(
+        rating = WebDriverWait(driver, timeout).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, 'span.a-icon-alt'))
         ).text.strip()
     except Exception as e:
@@ -63,7 +63,7 @@ def extract_amazon_reviews(url):
     
     # Extract total reviews
     try:
-        total_reviews_element = WebDriverWait(driver, 15).until(
+        total_reviews_element = WebDriverWait(driver, timeout).until(
             EC.presence_of_element_located((By.XPATH, "//span[contains(text(), 'global ratings') or contains(text(), 'customer reviews')]"))
         )
         total_reviews = total_reviews_element.text.strip()
